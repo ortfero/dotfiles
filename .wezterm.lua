8025159e41d8a487c45032dace548bacbae105c5
Andrei Ilin <ortfero@gmail.com>
2024-09-03 17:10:04 +0300

    Set cursor theme for wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,8 +1,10 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 config.font = wezterm.font('JetBrains Mono', { weight = 'Bold' })
-config.font_size = 13
+config.font_size = 11
 config.color_scheme = 'Gruvbox dark, soft (base16)'
 config.use_fancy_tab_bar = false
+config.xcursor_theme = 'simp1e-cursors-dark'
+config.xcursor_size = 32
 return config
 
