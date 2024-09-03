2c47399e29999f9b1f2cb718ed3c6d3bd4915798
Andrei Ilin <ortfero@gmail.com>
2024-09-03 17:56:11 +0300

    Increase wezterm font size

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 config.font = wezterm.font('JetBrains Mono', { weight = 'Bold' })
-config.font_size = 11
+config.font_size = 12
 config.color_scheme = 'Gruvbox dark, soft (base16)'
 config.use_fancy_tab_bar = false
 config.xcursor_theme = 'simp1e-cursors-dark'
